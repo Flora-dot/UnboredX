7f6e8e807eb7381d2fbf4739151b86e0cfc8fb7e
Flora Nwakpa <floranwakpa@gmail.com>
2024-02-05 02:37:58 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const getActivityButton = document.getElementById('get-activity');
+const activitycontainer = document.getElementById('container')
+const headingText = document.getElementById('heading');
+
+
+
+function toggleActivity() {
+    if(activitycontainer.style.display === 'none') {
+        activitycontainer.style.display = 'flex';
+        headingText.style.display = 'none';
+
+    } else {
+        activitycontainer.style.display = 'none';
+        headingText.style.display = 'block';
+    }
+}
+
+function fetchApi() {
+    fetch('http://www.boredapi.com/api/activity/')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        if(data.link){
+            activitycontainer.innerHTML = `<span class ='roboto'>Activity:</span><a href = ${data.link}><span class='montserrat bold animation'>${data.activity}</span>Learn more</a>`
+        }else{
+            activity.innerHTML = `<span class ='sixtyfour'>Activity: </span><span class='roboto '>${data.activity}</span>`;
+            type.innerHTML = `<span class ='sixtyfour'>Type: </span><span class='roboto'>${data.type}</span>`;
+            participants.innerHTML = `<span class ='sixtyfour'>Participants: </span><span class='roboto'>${data.participants}</span>`;
+            accessibility.innerHTML = `<span class ='sixtyfour'>Accessibilty: </span><span class='roboto'>${data.accessibility}</span>`;
+        }
+    });
+}
+
+function fetchActivity(){
+    if(activitycontainer.style.display = 'flex'){
+        activitycontainer.style.display = 'flex';
+        headingText.style.display = 'none';
+        fetchApi();
+    } else{
+        activitycontainer.style.display = 'none';
+        headingText.style.display = 'block';
+    }
+}
+
+
+ getActivityButton.addEventListener('click', toggleActivity,);
+//  getActivityButton.addEventListener('click', fetchApi);
+ getActivityButton.addEventListener('click', fetchActivity);
+
